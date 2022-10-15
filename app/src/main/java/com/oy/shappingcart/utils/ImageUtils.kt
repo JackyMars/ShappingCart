baedde11d176a834857cd29b0ff7dc48a8f6d97b
MarsYang <billyeric2010@gmail.com>
2022-10-15 13:19:08 +0800

    [VERSION:1.0.1][FUNCTION] load image function

---
+++ app/src/main/java/com/oy/shappingcart/utils/ImageUtils.kt
@@ -0,0 +1,81 @@
+package com.oy.shappingcart.utils
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.LocalContext
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.oy.shappingcart.R
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+const val DEFAULT_IMAGE = R.drawable.no_image
+
+@SuppressLint("UnrememberedMutableState")
+@ExperimentalCoroutinesApi
+@Composable
+fun loadPicture(url: String, @DrawableRes defaultImage: Int): MutableState<Bitmap?> {
+
+    val bitmapState: MutableState<Bitmap?> = mutableStateOf(null)
+
+    // show default image while image loads
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(defaultImage)
+        .centerCrop()
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onLoadCleared(placeholder: Drawable?) { }
+            override fun onResourceReady(
+                resource: Bitmap,
+                transition: Transition<in Bitmap>?
+            ) {
+                bitmapState.value = resource
+            }
+        })
+
+    // get network image
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(url)
+        .centerCrop()
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onLoadCleared(placeholder: Drawable?) { }
+            override fun onResourceReady(
+                resource: Bitmap,
+                transition: Transition<in Bitmap>?
+            ) {
+                bitmapState.value = resource
+            }
+        })
+
+    return bitmapState
+}
+@SuppressLint("UnrememberedMutableState")
+@ExperimentalCoroutinesApi
+@Composable
+fun loadPicture(url: String): MutableState<Bitmap?> {
+
+    val bitmapState: MutableState<Bitmap?> = mutableStateOf(null)
+
+    // get network image
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(url)
+        .centerCrop()
+        .error(DEFAULT_IMAGE)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onLoadCleared(placeholder: Drawable?) { }
+            override fun onResourceReady(
+                resource: Bitmap,
+                transition: Transition<in Bitmap>?
+            ) {
+                bitmapState.value = resource
+            }
+        })
+    return bitmapState
+}
